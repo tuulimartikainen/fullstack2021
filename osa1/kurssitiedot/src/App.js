2ba1dfcf8b7dba5637aea8281ba1d66ca2307f0f
Tuuli Martikainen <tuuli.martikainen@edu.karelia.fi>
2021-11-21 14:00:07 +0200

    1.5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -47,29 +47,29 @@ const Total = (props) => {
 
 const App = () => {
   // const-määrittelyt
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const parts = [part1, part2, part3]
-
-
 
   return (
     <div>
-        <Header name={course} />     
-        <Content parts={parts}/>
-        <Total parts={parts} />
+        <Header name={course.name} />     
+        <Content parts={course.parts}/>
+        <Total parts={course.parts} />
     </div>
   )
 }
