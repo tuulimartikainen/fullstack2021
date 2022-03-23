0d3144dd29923ee0a421a53562ed484e1f7aba5d
Tuuli Martikainen <tuuli.martikainen@edu.karelia.fi>
2022-03-23 08:43:17 +0200

    4.3 ja 4.4

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,7 +1,6 @@
 const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
 const config = require('./utils/config')
-//const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
