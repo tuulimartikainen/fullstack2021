2cf7b2aed141558f8b9811f9fdf4e984a01c3cfb
Tuuli Martikainen <tuuli.martikainen@edu.karelia.fi>
2022-03-24 14:18:37 +0200

    4.5

--- osa4/blogilista/tests/favouriteBlog.test.js
+++ osa4/blogilista/tests/favouriteBlog.test.js
@@ -61,24 +61,15 @@ const blogs = [
       __v: 0
     }  
   ]
+  describe('favouriteBlog', () => {
 
-  const emptylist = []
-
-describe('totalLikes', () => {
-
-      test('of empty list is zer0', () => {
-        const result = listHelper.totalLikes(emptylist)
-        expect(result).toBe(0)
+    test('of favourite blog', () => {
+      const result = listHelper.favouriteBlog(blogs)
+      expect(result).toEqual(
+          {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12
       })
-
-      test('when list has only one blog equals the likes of that', () => {
-        const result = listHelper.totalLikes(listWithOneBlog)
-        expect(result).toBe(5)
-      })
-
-      test('of a bigger list is calculated right', () => {
-        const result = listHelper.totalLikes(blogs)
-        expect(result).toBe(36)
-      })
+    })
 })
-
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -8,8 +8,23 @@ const dummy = (blogs) => {
     }
     return blogs.reduce(reducer, 0)
   }
+
+  const favouriteBlog = (blogs) => {
+    const favourite = (previousValue, currentValue) => {
+    
+        if (previousValue.likes < currentValue.likes)
+            return currentValue
+        else return {title: previousValue.title, 
+            author: previousValue.author, 
+            likes: previousValue.likes
+     }
+    }
+    return blogs.reduce(favourite, {likes: 0})
+  }
+
   
   module.exports = {
     dummy, 
     totalLikes,
+    favouriteBlog
   }
